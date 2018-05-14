296a56a5f58077ed5266794a24f35a2d35c2aa1e
Jarideno <jdenottelander@gmail.com>
2018-05-14 12:06:51 +0200

    database connectie
    database connectie gemaakt

---
+++ StudentenhuisAPI/Database/databaseconnection.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+
+
+let connection = mysql.createConnection({
+    //properties
+    host:"localhost",
+    user:"studentenhuis_user",
+    password:"secret",
+    database:"studentenhuis"
+});
+
+function connectToDB(){
+    connection.connect((error)=>{
+        if(error){
+            console.log(error);
+        } else {
+            console.log("Connected to DB!");
+        }
+    });
+};
+
+module.exports.connectToDB = connectToDB;
+module.exports.mysql = mysql;
+module.exports.connection = connection;
