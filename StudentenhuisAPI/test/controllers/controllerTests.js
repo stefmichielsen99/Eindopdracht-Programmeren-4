8f580c84132b1a092b37ac842086934bb8834131
Stef Michielsen <staffeltjenac@gmail.com>
2018-05-15 23:32:08 +0200

    RegistrationTest

--- StudentenhuisAPI/test/controllers/controllerTests.js
+++ StudentenhuisAPI/test/controllers/controllerTests.js
@@ -4,7 +4,56 @@
  *zullen optreden – bijvoorbeeld wanneer een gebruiker een maaltijd wil verwijderen die hij niet zelf
  *heeft aangemaakt, of een gebruiker zich aan wil melden voor een maaltijd maar reeds aangemeld is.
  */
-
+ const chai = require('chai');
  const assert = require('chai').assert;
  const controllerstud = require('../../controllers/studentenhuis_controller');
- const controllermltd = require()
+ const controllermltd = require('../../controllers/maaltijd_controller');
+ const controllerdeeln = require('../../controllers/deelnemer_controller');
+ const controllerauth = require('../../controllers/authentication_controller');
+
+ let validToken
+
+ describe('App', function(){
+     describe('ControllerStudentenhuis', () =>{
+         //Test Hier
+     });
+     describe('ControllerMaaltijd', () => {
+         //Test Hier
+     });
+     describe('ControllerDeelnemer', () => {
+        //Test Hier
+     });
+     describe('ControllerAuthentication', () => {
+        describe('Registration', () => {
+            it('should return a token when providing valid information', (done) => {
+                //Manier vinden om te testen / Hoe zie je of ie slaagt?
+                chai.request(server)
+                .post(endpoint)
+                .send({
+                    'Voornaam': ' FirstName ',
+                    'Achternaam': ' LastName ',
+                    'Email': 'tst@test.com',
+                    'Password': 'secret'
+                })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+
+                    const response = res.body;
+                    response.should.have.property('token').which.is.a('string');
+                    response.should.have.property('email').which.is.a('string');
+
+                    // Export the aquired token for other testcases (if necessary).
+                    validToken = res.body.token;
+                    module.exports = {
+                        token: validToken
+                    };
+                    done();
+                });
+            });
+        });
+            
+     });
+ });
+
+ 
