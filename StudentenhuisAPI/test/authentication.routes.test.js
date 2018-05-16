4c51a1b8bade6eaacf110b2f5e7d75528a23b4c2
Stef Michielsen <staffeltjenac@gmail.com>
2018-05-16 11:37:30 +0200

    2 oude testfiles verwijderd en de juiste zip geimplementeerd

---
+++ StudentenhuisAPI/test/authentication.routes.test.js
@@ -0,0 +1,133 @@
+/**
+ * Testcases aimed at testing the authentication process. 
+ */
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const server = require('../server')
+
+chai.should()
+chai.use(chaiHttp)
+
+// After successful registration we have a valid token. We export this token
+// for usage in other testcases that require login.
+let validToken
+
+describe('Registration', () => {
+    it('should return a token when providing valid information', (done) => {
+        chai.request(server)
+        .post(endpoint)
+        .send({
+            'Voornaam': ' FirstName ',
+            'Achternaam': ' LastName ',
+            'Email': 'tst@test.com',
+            'Password': 'secret'
+        })
+        .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+
+            const response = res.body;
+            response.should.have.property('token').which.is.a('string');
+            response.should.have.property('email').which.is.a('string');
+
+            // Export the aquired token for other testcases (if necessary).
+            validToken = res.body.token;
+            module.exports = {
+                token: validToken
+            };
+            done();
+        });
+
+    it('should return an error on GET request', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when the user already exists', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when no firstname is provided', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when firstname is shorter than 2 chars', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when no lastname is provided', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when lastname is shorter than 2 chars', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when email is invalid', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+})
+
+describe('Login', () => {
+
+    it('should return a token when providing valid information', (done) => {
+        chai.request(server)
+        .post('/api/login')
+        .send({
+            'Email': 'tst@test.com',
+            'Password': 'secret'
+        })
+        .end((err, res) => {
+            res.should.have.status(200)
+            res.body.should.be.a('object')
+            const response = res.body
+            response.should.have.property('token').which.is.a('string')
+            response.should.have.property('email').which.is.a('string')
+            done()
+        });
+    }); 
+
+    it('should throw an error when email does not exist', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when email exists but password is invalid', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+    it('should throw an error when using an invalid email', (done) => {
+        //
+        // Hier schrijf je jouw testcase.
+        //
+        done()
+    })
+
+})
+})
--- StudentenhuisAPI/test/routes/routesTests.js
+++
@@ -1,6 +0,0 @@
-/**
- * Voor de testen moeten er minimaal 7 tests worden geschreven en elke endpoint moet worden getest.
- * Hierbij test je niet alleen de verwachte correcte werking, maar je test vooral ook requests waarbij fouten
- *zullen optreden – bijvoorbeeld wanneer een gebruiker een maaltijd wil verwijderen die hij niet zelf
- *heeft aangemaakt, of een gebruiker zich aan wil melden voor een maaltijd maar reeds aangemeld is.
- */
