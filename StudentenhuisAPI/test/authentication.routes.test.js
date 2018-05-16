380c5ef3e71b0187447cff51c2f7b29ef2818b94
Stef Michielsen <staffeltjenac@gmail.com>
2018-05-16 14:16:51 +0200

    aanpassingen

--- StudentenhuisAPI/test/authentication.routes.test.js
+++ StudentenhuisAPI/test/authentication.routes.test.js
@@ -3,7 +3,7 @@
  */
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const server = require('../server')
+const server = require('../app')
 
 chai.should()
 chai.use(chaiHttp)
--- StudentenhuisAPI/test/studentenhuis.routes.test.js
+++ StudentenhuisAPI/test/studentenhuis.routes.test.js
@@ -1,6 +1,6 @@
 const chai = require('chai')
 const chaiHttp = require('chai-http')
-const server = require('../server')
+const server = require('../app')
 
 chai.should()
 chai.use(chaiHttp)
@@ -12,8 +12,6 @@ describe('Studentenhuis API POST', () => {
         .set('x-access-token', 'in.valid.token')
         .send({
             'firstname': '  FirstName  ',
-            'lastname': '  LastName   ',
-            'email': ' user@host.com ',
             'password': ' secret '
         })
         .end((err, res) => {
